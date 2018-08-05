b188a4a609c58177087961a630b65330891b6403
lioning <liang0356@sina.com>
2018-08-05 22:45:45 +0800

    采用asyncio库替代asyncore/asynchat
    ChatServer()
    	async ChatServer.run_server __init__(host, port, loop)
    			asyncio.start_server()
    	 async ChatServer.__handle_connect__()
    			ChatSession()
    
    AbstractAsyncioBase
    	AbstractAsyncioBase.__init__(reader, writer, loop)
    		run_coroutine_threadsafe(self.on_read, loop)
    	async AbstractAsyncioBase.on_read()
    		await self._reader.read()
    	async AbstractAsyncioBase.handle_write(data)
    		self._writer.write()
    		await self._writer.drain()
    
    AsyncioChat(AbstractAsyncioBase)
    	 AsyncioChat.__init__(reader, writer, loop)
    		AbstractAsyncioBase(....)
    	AsyncioChat.async on_read() 重载
    	 AsyncioChat.push()
    			Base.writer.write()
    	AsyncioChat.handle_read()
    			collect_incoming_data()
    			found_terminator()
    	AsyncioChat.handle_close()
    		Base.writer.close()
    	virtual AsyncioChat.collect_incoming_data()
    			raise error
    	virtual AsyncioChat.found_terminator() ??
    			raise error
    ChatSession(AsyncioChat)
    ChatSession.__init__(reader, writer) 修改

--- python/asyncio/text-chat/asyncio_chat_server.py
+++ python/asyncio/text-chat/asyncio_chat_server.py
@@ -1,43 +1,205 @@
-import asynchat
-import asyncore
+import asyncio
+import logging
+#import asynchat
+#import asyncore
 
+logging.basicConfig(level=logging.DEBUG,
+                format='%(asctime)s[%(funcName)s:%(lineno)d][%(levelname)s]:%(message)s',
+                datefmt='%a,%Y/%m/%d, %H:%M:%S')
 
-# 定义端口
-PORT = 6666
 
 # 定义结束异常类
 class EndSession(Exception):
     pass
 
+'''
+异步通信基类
+'''
+class AbstractAsyncioBase():
+    def __init__(self, reader, writer, loop):
+        logging.debug('%s:=======start========' %('AbstractAsyncioBase'))
+        self._reader = reader
+        self._writer = writer
+        self._loop   = loop
+        asyncio.run_coroutine_threadsafe(self.on_read(), loop)
 
-class ChatServer(asyncore.dispatcher):
+    async def on_read(self):
+        logging.debug('%s:=======start========' %('on_read'))
+        while True:
+            logging.debug("reading......")
+            line = await self._reader.read(1000)
+            if not line:
+                print ("[on_read]connection exit.\n")
+                break
+
+            print('[on_read]Recieve:%s' %(line.decode('utf-8').rstrip()))
+
+        logging.debug('%s:*******end*******' %('on_read'))
+
+class AsyncioChat(AbstractAsyncioBase):
+    ac_in_buffer_size = 65536
+    # we don't want to enable the use of encoding by default, because that is a
+    # sign of an application bug that we don't want to pass silently
+
+    use_encoding = 0
+    encoding = 'latin-1'
+
+    def __init__(self, reader, writer, loop):
+        AbstractAsyncioBase.__init__(self, reader, writer, loop)
+        # for string terminator matching
+        self.ac_in_buffer = b''
+
+    async def on_read(self):
+        logging.debug('%s:=======start========' %('AsyncioChat.on_read'))
+        while True:
+            logging.debug("reading......")
+            line = await self._reader.read(self.ac_in_buffer_size)
+            if not line:
+                print ("[on_read]connection exit.\n")
+                break
+
+            print('[AsyncioChat.on_read]Recieve:%s%s' %(line.decode('utf-8').rstrip(),'\n'))
+            self.handle_read(line)
+
+        logging.debug('%s:*******end*******' %('AsyncioChat.on_read'))
+
+    def set_terminator(self, term):
+        """Set the input delimiter.
+
+        Can be a fixed string of any length, an integer, or None.
+        """
+        if isinstance(term, str) and self.use_encoding:
+            term = bytes(term, self.encoding)
+        elif isinstance(term, int) and term < 0:
+            raise ValueError('the number of received bytes must be positive')
+        self.terminator = term
+
+    def get_terminator(self):
+        return self.terminator
+
+    def handle_close(self):
+        self._writer.close()
+
+    def handle_read(self, data):
+        logging.debug('%s:=======start========' %('AsyncioChat.handle_read'))
+        if isinstance(data, str) and self.use_encoding:
+            data = bytes(str, self.encoding)
+        self.ac_in_buffer = self.ac_in_buffer + data
+
+        # Continue to search for self.terminator in self.ac_in_buffer,
+        # while calling self.collect_incoming_data.  The while loop
+        # is necessary because we might read several data+terminator
+        # combos with a single recv(4096).
+
+        while self.ac_in_buffer:
+            lb = len(self.ac_in_buffer)
+            terminator = self.get_terminator()
+            if not terminator:
+                # no terminator, collect it all
+                self.collect_incoming_data(self.ac_in_buffer)
+                self.ac_in_buffer = b''
+            elif isinstance(terminator, int):
+                # numeric terminator
+                n = terminator
+                if lb < n:
+                    self.collect_incoming_data(self.ac_in_buffer)
+                    self.ac_in_buffer = b''
+                    self.terminator = self.terminator - lb
+                else:
+                    self.collect_incoming_data(self.ac_in_buffer[:n])
+                    self.ac_in_buffer = self.ac_in_buffer[n:]
+                    self.terminator = 0
+                    self.found_terminator()
+            else:
+                # 3 cases:
+                # 1) end of buffer matches terminator exactly:
+                #    collect data, transition
+                # 2) end of buffer matches some prefix:
+                #    collect data to the prefix
+                # 3) end of buffer does not match any prefix:
+                #    collect data
+                terminator_len = len(terminator)
+                index = self.ac_in_buffer.find(terminator)
+                if index != -1:
+                    # we found the terminator
+                    if index > 0:
+                        # don't bother reporting the empty string
+                        # (source of subtle bugs)
+                        self.collect_incoming_data(self.ac_in_buffer[:index])
+                    self.ac_in_buffer = self.ac_in_buffer[index+terminator_len:]
+                    # This does the Right Thing if the terminator
+                    # is changed here.
+                    self.found_terminator()
+                else:
+                    # check for a prefix of the terminator
+                    index = find_prefix_at_end(self.ac_in_buffer, terminator)
+                    if index:
+                        if index != lb:
+                            # we found a prefix, collect up to the prefix
+                            self.collect_incoming_data(self.ac_in_buffer[:-index])
+                            self.ac_in_buffer = self.ac_in_buffer[-index:]
+                        break
+                    else:
+                        # no prefix, collect it all
+                        self.collect_incoming_data(self.ac_in_buffer)
+                        self.ac_in_buffer = b''
+        logging.debug('%s:*******end*******' %('AsyncioChat.handle_read'))
+
+
+    def collect_incoming_data(self, data):
+        raise NotImplementedError("must be implemented in subclass")
+
+    def found_terminator(self):
+        raise NotImplementedError("must be implemented in subclass")
+
+    def push(self, data):
+        logging.debug('%s:=======start========' %('push'))
+        logging.debug('data:%s' %(data))
+        self._writer.write(data)
+        logging.debug('%s:*******end*******' %('push'))
+
+# this could maybe be made faster with a computed regex?
+# [answer: no; circa Python-2.0, Jan 2001]
+# new python:   28961/s
+# old python:   18307/s
+# re:        12820/s
+# regex:     14035/s
+
+def find_prefix_at_end(haystack, needle):
+    l = len(needle) - 1
+    while l and not haystack.endswith(needle[:l]):
+        l -= 1
+    return l
+
+class ChatServer():
     """
     聊天服务器
     """
+    def __init__(self, host, port, loop):
 
-    def __init__(self, port):
-        asyncore.dispatcher.__init__(self)
-        # 创建socket
-        self.create_socket()
-        # 设置 socket 为可重用
-        self.set_reuse_addr()
-        # 监听端口
-        self.bind(('', port))
-        self.listen(5)
-        self.users = {}
+        self.co_srv = None
+        self._host  = host
+        self._port  = port
+        self._loop  = loop
+        self.users  = {}
         self.main_room = ChatRoom(self)
 
-    def handle_accept(self):
-        sock, addr = self.accept() ####基类有epoll的loop
-        ChatSession(self, sock)
+    async def run_server(self):
+        self.co_srv= await asyncio.start_server(self.handle_accept, self._host, self._port, loop=loop)
+        print('Server started at http://%s:%s...'%(self._host, self._port))
 
-class ChatSession(asynchat.async_chat):
+    def handle_accept(self, reader, writer):
+        logging.debug('%s:=======start========' %('handle_accept'))
+        ChatSession(self, reader, writer, self._loop)
+        logging.debug('%s:*******end********' %('handle_accept'))
+
+class ChatSession(AsyncioChat):
     """
     负责和客户端通信
     """
 
-    def __init__(self, server, sock):
-        asynchat.async_chat.__init__(self, sock)
+    def __init__(self, server, reader, writer, loop):
+        AsyncioChat.__init__(self, reader, writer, loop)
         self.server = server
         self.set_terminator(b'\n')
         self.data = []
@@ -49,16 +211,7 @@ class ChatSession(asynchat.async_chat):
         # 从当前房间移除自身，然后添加到指定房间。
         self.leave_current_room();
         self.enter_room(room);
-        '''
-        try:
-            cur_room = self.room
-        except AttributeError:
-            pass
-        else:
-            cur_room.remove(self)
-        self.room = room
-        room.add(self)
-        '''
+
     def leave_current_room(self):
         try:
             cur_room = self.room
@@ -77,6 +230,7 @@ class ChatSession(asynchat.async_chat):
 
     def found_terminator(self):
         # 当客户端的一条数据结束时的处理
+        logging.debug("data=%s" % (self.data))
         line = ''.join(self.data)
         self.data = []
         try:
@@ -87,8 +241,13 @@ class ChatSession(asynchat.async_chat):
 
     def handle_close(self):
         # 当 session 关闭时，将进入 LogoutRoom
-        asynchat.async_chat.handle_close(self)
+        logging.debug("handle_close")
+
+        AsyncioChat.handle_close(self)
+        #asynchat.async_chat.handle_close(self)
         self.enter(LogoutRoom(self.server))
+
+        logging.debug("handle_close end")
 
 class CommandHandler:
     """
@@ -142,6 +301,7 @@ class Room(CommandHandler):
             session.push(line)
 
     def do_logout(self, session, line):
+        logging.debug("logout......")
         # 退出房间
         raise EndSession
 
@@ -213,10 +373,11 @@ class ChatRoom(Room):
             session.push((other.name + '\n').encode("utf-8"))
 
 if __name__ == '__main__':
-
-    s = ChatServer(PORT)
     try:
-        print("chat serve run at '0.0.0.0:{0}'".format(PORT))
-        asyncore.loop()
+        loop = asyncio.get_event_loop()
+
+        s = ChatServer('127.0.0.1','8000',loop)
+        loop.run_until_complete(s.run_server())
+        loop.run_forever()
     except KeyboardInterrupt:
         print("chat server exit")
