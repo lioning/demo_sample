cc0473de2dd4a53b05787b7e0630eaf3930b1ce1
lioning <liang0356@sina.com>
2018-08-31 22:25:29 +0800

    更新python日志格式化输出
    增加颜色

--- python/log-unittest-sample-demo.py
+++ python/log-unittest-sample-demo.py
@@ -18,11 +18,20 @@ def dump(obj):
 
 logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s[%(funcName)s:%(lineno)d][%(levelname)s]:%(message)s',
-                #format='%(filename)s',
                 datefmt='%a,%Y/%m/%d, %H:%M:%S')
                 #datefmt='%a, %d %b %Y %H:%M:%S',
+#format 支持的变量
+#    文件名 %(filename)s
+#指定日志文件名和打开模式
                 #filename='myapp.log',
                 #filemode='w')
+#高亮绿色字的函数名
+#format='\033[1;32m%(funcName)s\033[0m',
+
+#带颜色的显示
+#    命令 \033[ 开始，后接操作代码。操作颜色的操作代码以 m 结尾。多个操作代码间用 ; 分隔，且只能最后一个代码以 m 结尾。
+#        如，\033[31m 表示红色字，\033[01;31m 表示高亮红色字，\033[0m 表示关闭属性设置
+
 
 #没有@functools.wraps(func)，被装饰的函数内部使用__name__的结果将会是wrapper
 '''
